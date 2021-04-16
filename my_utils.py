bb6edc85d94b9316d54d636b0e245aacac63ece7
jeevesh8 <creativityinczenyoga@gmail.com>
2021-04-16 09:06:47 +0530

    chiisana hosei

--- my_utils.py
+++ my_utils.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+from functools import wraps
 
 def convert_outputs_to_tensors(dtype):
     def inner(func):
+        @wraps(func)
         def tf_func(*args, **kwargs):
             outputs = func(*args, **kwargs)
             return (tf.convert_to_tensor(elem, dtype=dtype) for elem in outputs)
