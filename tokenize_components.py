e7a5d575670b160e3e16224fabd33c2aebe66326
jeevesh8 <creativityinczenyoga@gmail.com>
2021-04-16 08:43:04 +0530

    chiisana hosei

--- tokenize_components.py
+++ tokenize_components.py
@@ -123,7 +123,7 @@ def get_thread_with_labels(filename):
         begin, end = begin_positions[comp_id], end_positions[comp_id]
         comp_type_labels[begin:end] = get_arg_comp_lis(comp_types[comp_id], end-begin)
         relation_type_labels[begin] = config['relations'].index(str(rel))
-        relation_type_labels[begin+1:end] = config['relations'].index('cont')
+        relation_type_labels[begin+1:end] = [config['relations'].index('cont')]*(end-(begin+1))
         for j, ref_id in enumerate(ref.split('_')):
             rel_dist = begin_positions[ref_id]-prev_comment_begin_position[comp_id]
             comp_refer_labels = get_ref_link_lis(rel_dist, begin-prev_comment_begin_position[comp_id], end-prev_comment_begin_position[comp_id])
