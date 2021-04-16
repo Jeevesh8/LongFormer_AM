1158bb85228b5378372df817043f73aad3a81dbb
jeevesh8 <creativityinczenyoga@gmail.com>
2021-04-16 08:19:42 +0530

    chiisana hosei

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ config = {
                    'undercutter_attack', 'disagreement', 
                    'rebuttal', 'support', 
                    'cont', 'None', 
-                   'partial_agreement', 'partial_disagreement']
+                   'partial_agreement', 'partial_disagreement'],
     'arg_components' : {'other': 0, 'B-C' : 1, 'I-C' : 2, 'B-P' : 3, 'I-P' : 4},
     'max_tokenizer_length': 8192+2,
     'max_rel_comps' : 5,                                                                        #The maximum number of components a component can be related to
