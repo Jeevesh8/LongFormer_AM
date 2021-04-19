9518a904e7f30612b5dbb8ac4511dd81cb8d51dc
jeevesh8 <creativityinczenyoga@gmail.com>
2021-04-19 22:09:36 +0530

    chiisana hosei

--- models/TaskModel.py
+++ models/TaskModel.py
@@ -97,7 +97,7 @@ class TaskModel(tf.keras.models.Model):
         comp_type_cc_loss = self.get_cross_entropy(logits, comp_type_labels, config['pad_for']['comp_type_labels'])
         relation_type_cc_loss = self.get_cross_entropy(relation_type_logits, relation_type_labels, config['pad_for']['relation_type_labels'])
         refers_cc_losses = tf.map_fn(lambda labels: self.get_cross_entropy(refers_logits, labels, config['pad_for']['refers_labels']), tf.transpose(refers_labels, perm=(2,0,1)), fn_output_signature=None)
-        refers_cc_loss = tf.reduce_sum(refers_labels_cc_losses)
+        refers_cc_loss = tf.reduce_sum(refers_cc_losses)
         return tf.reduce_mean(crf_loss), comp_type_cc_loss, relation_type_cc_loss, refers_cc_loss
     
     def infer_step(self, x):
