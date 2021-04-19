1bad56b3c9bb6b60c443a31f53a9d1089ebc4a42
jeevesh8 <creativityinczenyoga@gmail.com>
2021-04-19 22:22:12 +0530

    chiisana hosei

--- models/TaskModel.py
+++ models/TaskModel.py
@@ -88,7 +88,7 @@ class TaskModel(tf.keras.models.Model):
         sample_weight = self.compute_batch_sample_weight(labels, pad_label)
         cc_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
         n_samples = tf.reduce_sum(sample_weight)
-        return tf.reduce_sum(cc_loss*sample_weight)/n_samples if n_samples!=0 else tf.conver_to_tensor([0.])
+        return tf.reduce_sum(cc_loss*sample_weight)/n_samples if n_samples!=0 else tf.convert_to_tensor([0.])
     
     def compute_loss(self, x, y):
         comp_type_labels, relation_type_labels, refers_labels = y
