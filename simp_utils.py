673d5f0164725865301b948fc096d6c93e5fe67e
jeevesh8 <creativityinczenyoga@gmail.com>
2021-05-05 14:26:07 +0530

    chiisana hosei

--- simp_utils.py
+++ simp_utils.py
@@ -1,9 +1,10 @@
-from cofigs import config
+from configs import config
 
 def labels_to_tags(labels):
     """Converts integer labels to component type labels.
     Input: array of integer labels 
     Output: arry of string tags
     """
-    tag_dict = {v:k for k, v in config['arg_components']}
+    tag_dict = {v:k for k, v in config['arg_components'].items()}
+    tag_dict[len(tag_dict)] = 'pad'
     return [tag_dict[i] for i in labels]
